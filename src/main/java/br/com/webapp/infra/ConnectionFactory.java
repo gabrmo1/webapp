ef361888929edbca94dc6c6b78385c2355e54695
gabrmo1 <gabrielmoreno1jlle@gmail.com>
2023-05-22 14:31:21 -0300

    Setting database properties through pom file

--- src/main/java/br/com/webapp/infra/ConnectionFactory.java
+++ src/main/java/br/com/webapp/infra/ConnectionFactory.java
@@ -1,16 +1,37 @@
 package br.com.webapp.infra;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.project.MavenProject;
+
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
-    private ConnectionFactory(){}
-
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp", "root", "1nd2gw3jmm");
+            Model model = null;
+            FileReader reader;
+            MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+
+            try {
+                reader = new FileReader("pom.xml");
+                model = mavenreader.read(reader);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            MavenProject project = new MavenProject(model);
+            Properties properties = project.getProperties();
+            String url = properties.getProperty("url.database");
+            String user = properties.getProperty("user.database");
+            String password = properties.getProperty("password.database");
+
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
